b20158d78679b9ede398c76e70c82c51f71cf551
jq <pierwszy.jakubowiczor@gmail.com>
2024-04-07 21:46:18 +0200

    TypeScript Complete!

--- src/scripts_ts/html_elements.ts
+++ src/scripts_ts/html_elements.ts
@@ -12,15 +12,21 @@ var learning_phase_panel = document.getElementById("learning-phase-panel")!
 var learning_phase_fields = document.getElementById("learning-phase-fields")!
 
 var pause_time_panel = document.getElementById("pause-time-panel")!
-var edit_pause_minute = document.getElementById("edit-pause-minute")!
-var edit_pause_second = document.getElementById("edit-pause-second")!
+var pause_editor_minute = document.getElementById("pause-editor-minute")! as HTMLInputElement
+var pause_editor_second = document.getElementById("pause-editor-second")! as HTMLInputElement
 var pause_minute = document.getElementById("pause-minute")!
 var pause_second = document.getElementById("pause-second")!
+var pause_editor_row = document.getElementById("pause-editor-row")!
+var pause_show_row = document.getElementById("pause-show-row")!
+var cancel_pause_button = document.getElementById("cancel-pause-button")! as HTMLButtonElement
+var start_pause_button = document.getElementById("start-pause-button")! as HTMLButtonElement
 
 var testing_phase_panel = document.getElementById("testing-phase-panel")!
 var testing_countdown_minute = document.getElementById("testing-countdown-minute")!
 var testing_countdown_second = document.getElementById("testing-countdown-second")!
 var testing_table = document.getElementById("testing-table")!
+var testing_panel_buttons = document.getElementById("testing-panel-buttons")!
+var check_results_panel_buttons = document.getElementById("check-results-panel-buttons")!
 
 
 var global_loading = document.getElementById("global-loading")!
--- src/scripts_ts/main.ts
+++ src/scripts_ts/main.ts
@@ -1,7 +1,60 @@
 var last_practicing_file: string | null = null
 
-function openLastFile() {
+var test_timer_interval_id: number | null = null
+var test_timer_start_millis: number | null = null
 
+var pause_interval_id: number | null = null;
+var pause_start_millis: number | null = null;
+var pause_end_millis: number | null = null;
+
+function nowUtcMillis() {
+    let date = new Date();
+    //Converted From Minutes To Milliseconds
+    let timezoneDiff = date.getTimezoneOffset() * 60 * 1000
+    //If you're time is later than UTC then timezoneDiff is negative
+    return date.getTime() + timezoneDiff
+}
+
+async function openFile(file_path: string) {
+    await open_file(file_path)
+    if (only_one_selection != null) {
+        switch (only_one_selection) {
+            case 0:
+                notDoneSelect();
+                break;
+            case 1:
+                doneSelect();
+                break;
+            default:
+                console.log(`only_one_selection: ${only_one_selection}`);
+                showError("Unknown default subject type detected!");
+                openTypeSelector();
+        }
+    } else {
+        openTypeSelector();
+    }
+}
+
+function openLastFile() {
+    let action = async () => {
+        if (last_practicing_file != null) {
+            openFile(last_practicing_file)
+        } else {
+            showError("There is no last file open!")
+        }
+    }
+
+    let run = () => {
+        startGlobalLoading();
+        action().then(() => {
+            stopGlobalLoading();
+        }, (err) => {
+            stopGlobalLoading();
+            showError(err, run)
+        })
+    }
+
+    run();
 }
 
 function openFileSelector() {
@@ -13,23 +66,7 @@ function openFileSelector() {
         while (selected == null) {
             selected = <string | null>await windowFileOpen(last_practicing_file, "Open file with practice data")
             if (selected != null) {
-                await open_file(selected)
-                if (only_one_selection != null) {
-                    switch (only_one_selection) {
-                        case 0:
-                            notDoneSelect();
-                            break;
-                        case 1:
-                            doneSelect();
-                            break;
-                        default:
-                            console.log(`only_one_selection: ${only_one_selection}`);
-                            showError("Unknown default subject type detected!");
-                            openTypeSelector();
-                    }
-                } else {
-                    openTypeSelector();
-                }
+                openFile(selected)
             }
         }
 
@@ -84,45 +121,132 @@ function notDoneSelect() {
     subjectSelected(false);
 }
 
+
+function testTimerUpdate() {
+    let now = nowUtcMillis();
+    let diff = now - test_timer_start_millis!
+
+    let current_timer_millis = 600_000 - diff
+
+    let millis = current_timer_millis % 1000;
+    let seconds = (current_timer_millis - millis) / 1000;
+    let shown_seconds = seconds % 60;
+    if (seconds < 0) {
+        shown_seconds = (-seconds) % 60;
+    }
+    let minutes = (seconds - (seconds % 60)) / 60;
+
+
+    testing_countdown_minute.innerText = minutes.toString();
+    testing_countdown_second.innerText = shown_seconds.toString().padStart(2, "0");
+}
+
 function startTest() {
     hide(learning_phase_panel);
     show(testing_phase_panel);
-    //TODO Reset And Start 10 Minute Timer
-    //TODO Reset Test Buttons shown
-    //TODO Hide Results
+    //Reset And Start 10 Minute Timer Countdown
+    testing_countdown_minute.innerText = "10";
+    testing_countdown_second.innerText = "00";
+
+    test_timer_start_millis = nowUtcMillis();
+    //Start Check every 100ms, if it wasn't activated before
+    if (test_timer_interval_id == null) {
+        test_timer_interval_id = setInterval(() => {
+            testTimerUpdate();
+        }, 100)
+    }
+
+    //Reset Test Buttons shown
+    testing_panel_buttons.style.display = "";
+    //Hide Results
+    check_results_panel_buttons.style.display = "none";
+    testing_table.classList.add("second-hidden");
 
 }
 function waitStartTest() {
     hide(learning_phase_panel);
     show(pause_time_panel);
-    //TODO Reset Pause Time Selector
+    //Reset Pause Time Selector
+    pause_editor_row.style.display = ""
+    pause_show_row.style.display = "none"
 
 }
 function cancelPause() {
-    //TODO Go back to learning panel
+    pause_editor_row.style.display = ""
+    pause_show_row.style.display = "none"
+    cancel_pause_button.onclick = exitPauseMenu;
+    start_pause_button.disabled = false;
+    if (pause_interval_id != null) {
+        clearInterval(pause_interval_id);
+        pause_interval_id = null
+    }
 
 }
+function exitPauseMenu() {
+    show(learning_phase_panel);
+    hide(pause_time_panel);
+}
+
+function pauseTimerUpdate() {
+    let now = nowUtcMillis();
+
+    if (now > pause_end_millis!) {
+        //Pause is complete
+        clearInterval(pause_interval_id!);
+        pause_interval_id = null;
+        hide(pause_time_panel);
+        startTest();
+        return;
+    }
+
+    let current_timer_millis = pause_end_millis! - now
+
+    let millis = current_timer_millis % 1000;
+    let seconds = (current_timer_millis - millis) / 1000;
+    let shown_seconds = seconds % 60;
+    if (seconds < 0) {
+        shown_seconds = (-seconds) % 60;
+    }
+    let minutes = (seconds - (seconds % 60)) / 60;
+
+
+    pause_minute.innerText = minutes.toString();
+    pause_second.innerText = shown_seconds.toString().padStart(2, "0");
+}
+
 function startPause() {
-    //TODO Hide Time Selector Inputs
-    //TODO Show And Start Pause Countdown
-    //TODO Disable start Pause Button
-    //TODO Switch Cancel pause button functionality (from exit to stop pause)
+    //Hide Time Selector Inputs
+    pause_editor_row.style.display = "none"
+    //Show And Start Pause Countdown
+    pause_show_row.style.display = ""
+    pause_start_millis = nowUtcMillis();
+    let pause_len_millis = (pause_editor_minute.valueAsNumber * 60_000) + (pause_editor_second.valueAsNumber * 1_000);
 
-}
+    pause_end_millis = pause_start_millis + pause_len_millis;
+    pause_interval_id = setInterval(() => {
+        pauseTimerUpdate();
+    }, 100)
+    //Disable start Pause Button
+    start_pause_button.disabled = true;
+    //Switch Cancel pause button functionality (from exit to stop pause)
+    cancel_pause_button.onclick = cancelPause;
 
-function pauseEnd() {
-    //TODO Hide Pause Selector Menu
-    startTest();
 }
 
 function backToLearning() {
-    //TODO Hide Testing Panel
-    //TODO Show Learning Panel
+    hide(testing_phase_panel);
+    show(learning_phase_panel);
 
 }
 function completeLearning() {
-    //TODO Show Results
-    //TODO Show Check Result Buttons Row
+    //Hide Testing Buttons
+    testing_panel_buttons.style.display = "none";
+    //Show Results
+    check_results_panel_buttons.style.display = "";
+    testing_table.classList.remove("second-hidden");
+    //Pause Countdown Timer
+    clearInterval(test_timer_interval_id!);
+    test_timer_interval_id = null;
 }
 function learnAgain() {
     backToLearning();
